41970a8fa0683103edb725c407489429d0cbbcd8
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2024-06-05 15:03:21 +0100

    made a fix

--- dist/controllers/api_controllers.js
+++ dist/controllers/api_controllers.js
@@ -9,12 +9,14 @@ exports.api_test = api_test;
 const sendMail = async (req, res) => {
     try {
         const formData = req.body;
+        if (!formData)
+            return res.status(400).json({ message: 'No data provided!' });
         const mailer = await transportMail(formData);
         res.status(200).json({ message: 'Mail sent successfully!', messageId: mailer.messageId });
     }
     catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Mail not sent!' });
+        res.status(500).json({ error: 'Mail not sent!', message: error });
     }
 };
 exports.sendMail = sendMail;
--- src/controllers/api_controllers.ts
+++ src/controllers/api_controllers.ts
@@ -8,11 +8,12 @@ const api_test = (req: Request, res: Response) => {
 const sendMail = async (req: Request, res: Response) => {
   try {
     const formData = req.body;
+    if (!formData) return res.status(400).json({ message: 'No data provided!' });
     const mailer = await transportMail(formData);
     res.status(200).json({ message: 'Mail sent successfully!', messageId: mailer.messageId });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Mail not sent!' });
+    res.status(500).json({ error: 'Mail not sent!', message: error });
   }
 };
 
