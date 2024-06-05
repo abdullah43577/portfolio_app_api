ecbf3ce40ee34380c7da595a6ed33b70d0f86f2c
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2024-06-05 15:44:43 +0100

    bug fixes

--- dist/controllers/api_controllers.js
+++ dist/controllers/api_controllers.js
@@ -9,14 +9,16 @@ exports.api_test = api_test;
 const sendMail = async (req, res) => {
     try {
         const formData = req.body;
-        if (!formData)
-            return res.status(400).json({ message: 'No data provided!' });
+        if (hasEmptyStrings(formData))
+            return res.status(400).json({ message: 'Empty fields not allowed!' });
         const mailer = await transportMail(formData);
         res.status(200).json({ message: 'Mail sent successfully!', messageId: mailer.messageId });
     }
     catch (error) {
-        console.error(error);
         res.status(500).json({ error: 'Mail not sent!', message: error });
     }
 };
 exports.sendMail = sendMail;
+function hasEmptyStrings(obj) {
+    return Object.values(obj).some((value) => value === '');
+}
--- src/controllers/api_controllers.ts
+++ src/controllers/api_controllers.ts
@@ -8,13 +8,16 @@ const api_test = (req: Request, res: Response) => {
 const sendMail = async (req: Request, res: Response) => {
   try {
     const formData = req.body;
-    if (!formData) return res.status(400).json({ message: 'No data provided!' });
+    if (hasEmptyStrings(formData)) return res.status(400).json({ message: 'Empty fields not allowed!' });
     const mailer = await transportMail(formData);
     res.status(200).json({ message: 'Mail sent successfully!', messageId: mailer.messageId });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: 'Mail not sent!', message: error });
   }
 };
 
 export { api_test, sendMail };
+
+function hasEmptyStrings(obj: any) {
+  return Object.values(obj).some((value) => value === '');
+}
